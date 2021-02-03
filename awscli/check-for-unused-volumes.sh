c2a0ddb492ea1cbdcce3e108f48f0d020102d21e
Mike Hicklen <>
2021-02-02 18:00:49 -0600

    Using jq instead of shell expansion and awk

--- awscli/check-for-unused-volumes.sh
+++ awscli/check-for-unused-volumes.sh
@@ -3,9 +3,10 @@
 for profile in {default,prod,qa,dev,sandbox}
 do
     echo -e "ENV: ${profile}"
-    for region in {ap-northeast-1,ap-northeast-2,ap-south-1,ap-southeast-1,ap-southeast-2,ca-central-1,eu-central-1,eu-west-1,eu-west-2,sa-east-1,us-east-1,us-east-2,us-west-1,us-west-2}
+    for region in $(aws ec2 describe-regions | jq '.Regions[].RegionName' -r)
     do
         echo -e "REGION: ${region}"
-        aws --profile ${profile} --region ${region} ec2 describe-volumes --filters "Name=status,Values=available" | awk -F: '/(VolumeType|VolumeId|State|CreateTime|Size)/ {print $1 ": " $2}' | awk ' {print;} NR % 5 == 0 { print "            --------------------------------"; }'
+        aws --profile ${profile} --region ${region} ec2 describe-volumes --filters "Name=status,Values=in-use" | jq '.Volumes[] | { name:(.Tags[]? | select(.Key=="Name") | .Value) }, .Attachments[].InstanceId, .Attachments[].VolumeId, .AvailabilityZone, .Encrypted, .Size, .VolumeType, .State, .CreateTime' -r
     done
 done
+
